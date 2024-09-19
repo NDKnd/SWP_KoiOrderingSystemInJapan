52b5c4695c88ef93e02512b8fe60968fbc47d0b8
HienDamHai <hiendamhai12b6@gmail.com>
2024-09-19 09:52:08 +0700

    add by Hien

--- src/Pages/Login/LoginForm.jsx
+++ src/Pages/Login/LoginForm.jsx
@@ -13,65 +13,69 @@ const LoginForm = () => {
         setAction('');
     }
 
+    
 
-    return (
-        <div className={`wrapper${action}`}>
-            <div className="form-box login">
-                <form action="">
-                    <h1>Login</h1>
-                    <div className="input-box">
-                        <input type="text" placeholder="Username" required></input>
-                        <FaUser className="icon"></FaUser>
-                    </div>
-                    <div className="input-box">
-                        <input type="password" placeholder="Password" required></input>
-                        <FaLock className="icon"></FaLock>
-                    </div>
-                    <div className="remember-forgot">
-                        <label><input type="checkbox"></input>Remember me</label>
-                        <a href="#">Forgot password?</a>
-                    </div>
+    return (<>
+        <div className="login-body">
+            <div className={`wrapper${action}`}>
+                <div className="form-box login">
+                    <form action="">
+                        <h1>Login</h1>
+                        <div className="input-box">
+                            <input type="text" placeholder="Username" required></input>
+                            <FaUser className="icon"></FaUser>
+                        </div>
+                        <div className="input-box">
+                            <input type="password" placeholder="Password" required></input>
+                            <FaLock className="icon"></FaLock>
+                        </div>
+                        <div className="remember-forgot">
+                            <label><input type="checkbox"></input>Remember me</label>
+                            <a href="#">Forgot password?</a>
+                        </div>
 
-                    <button type="submit">Login</button>
-                    <div className="register-link">
-                        <p>Don't have an account? <a href="#" onClick={registerLink}>Register</a></p>
-                    </div>
-                </form>
-            </div>
+                        <button type="submit">Login</button>
+                        <div className="register-link">
+                            <p>Don't have an account? <a href="#" onClick={registerLink}>Register</a></p>
+                        </div>
+                    </form>
+                </div>
 
-            <div className="form-box register">
-                <form action="">
-                    <h1>Registraion</h1>
-                    <div className="input-box">
-                        <input type="text" placeholder="Username" required></input>
-                        <FaUser className="icon"></FaUser>
-                    </div>
-                    <div className="input-box">
-                        <input type="email" placeholder="Email" required></input>
-                        <FaEnvelope className="icon"></FaEnvelope>
-                    </div>
-                    <div className="input-box">
-                        <input type="password" placeholder="Password" required></input>
-                        <FaLock className="icon"></FaLock>
-                    </div>
-                    <div className="input-box">
-                        <input type="password" placeholder="Confirm password" required></input>
-                        <FaLock className="icon"></FaLock>
-                    </div>
-                    <div className="remember-forgot">
-                        <label><input type="checkbox" required></input>
-                            I agree to the terms & conditions
-                        </label>
-                    </div>
+                <div className="form-box register">
+                    <form action="">
+                        <h1>Registraion</h1>
+                        <div className="input-box">
+                            <input type="text" placeholder="Username" required></input>
+                            <FaUser className="icon"></FaUser>
+                        </div>
+                        <div className="input-box">
+                            <input type="email" placeholder="Email" required></input>
+                            <FaEnvelope className="icon"></FaEnvelope>
+                        </div>
+                        <div className="input-box">
+                            <input type="password" placeholder="Password" required></input>
+                            <FaLock className="icon"></FaLock>
+                        </div>
+                        <div className="input-box">
+                            <input type="password" placeholder="Confirm password" required></input>
+                            <FaLock className="icon"></FaLock>
+                        </div>
+                        <div className="remember-forgot">
+                            <label><input type="checkbox" required></input>
+                                I agree to the terms & conditions
+                            </label>
+                        </div>
 
-                    <button type="submit">Register</button>
-                    <div className="register-link">
-                        <p>Already have an account? <a href="#" onClick={loginLink}>Login</a></p>
-                    </div>
-                </form>
+                        <button type="submit">Register</button>
+                        <div className="register-link">
+                            <p>Already have an account? <a href="#" onClick={loginLink}>Login</a></p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    );
+
+    </>);
 }
 
 export default LoginForm
