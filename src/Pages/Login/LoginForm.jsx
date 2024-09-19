d7a3577c5aeff529f197dca78482194ebc7cc2da
abc@gmail.com <khoind2004@gmail.com>
2024-09-19 10:18:18 +0700

    Shorten the incon

--- src/Pages/Login/LoginForm.jsx
+++ src/Pages/Login/LoginForm.jsx
@@ -46,19 +46,19 @@ const LoginForm = () => {
                         <h1>Registraion</h1>
                         <div className="input-box">
                             <input type="text" placeholder="Username" required></input>
-                            <FaUser className="icon"></FaUser>
+                            <FaUser className="icon" />
                         </div>
                         <div className="input-box">
                             <input type="email" placeholder="Email" required></input>
-                            <FaEnvelope className="icon"></FaEnvelope>
+                            <FaEnvelope className="icon" />
                         </div>
                         <div className="input-box">
                             <input type="password" placeholder="Password" required></input>
-                            <FaLock className="icon"></FaLock>
+                            <FaLock className="icon" />
                         </div>
                         <div className="input-box">
                             <input type="password" placeholder="Confirm password" required></input>
-                            <FaLock className="icon"></FaLock>
+                            <FaLock className="icon" />
                         </div>
                         <div className="remember-forgot">
                             <label><input type="checkbox" required></input>
