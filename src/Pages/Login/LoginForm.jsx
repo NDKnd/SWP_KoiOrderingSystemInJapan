2624c9056cf14de51cf2a9b11ab523df34cc3b47
abc@gmail.com <khoind2004@gmail.com>
2024-09-24 21:29:11 +0700

    somethign api

--- src/Pages/Login/LoginForm.jsx
+++ src/Pages/Login/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './LoginForm.css'
 import { FaUser, FaEye, FaEyeSlash, FaEnvelope, FaPen, FaLock } from "react-icons/fa"
-import api from "./axios";
+import api from "./../../api/axios";
 import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
