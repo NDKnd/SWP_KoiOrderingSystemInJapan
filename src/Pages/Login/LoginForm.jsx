f81301431901f8899ca4f368e6f13222c6d9cfc4
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2024-09-25 11:18:43 +0700

    cap nhat axios

--- src/Pages/Login/LoginForm.jsx
+++ src/Pages/Login/LoginForm.jsx
@@ -1,179 +1,230 @@
 import React, { useEffect, useState } from "react";
-import './LoginForm.css'
-import { FaUser, FaEye, FaEyeSlash, FaEnvelope, FaPen, FaLock } from "react-icons/fa"
+import "./LoginForm.css";
+import {
+  FaUser,
+  FaEye,
+  FaEyeSlash,
+  FaEnvelope,
+  FaPen,
+  FaLock,
+} from "react-icons/fa";
 import api from "./../../services/axios";
 import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+  //for tranfsition from login to regis
+  const [action, setAction] = useState("");
+  const registerLink = () => {
+    setAction("active");
+  };
+  const loginLink = () => {
+    setAction("");
+  };
 
-    //for tranfsition from login to regis
-    const [action, setAction] = useState('');
-    const registerLink = () => {
-        setAction('active');
+  // for seeing Password
+  const [see, setSee] = useState(true);
+  const togglePassword = (event) => {
+    setSee((prevState) => !prevState);
+  };
+  useEffect(() => {
+    setSee();
+  }, [setSee]);
+  // for Confirm Password
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleConfirmPasswordChange = (e) => {
+    const value = e.target.value;
+    setConfirmPassword(value);
+
+    if (password !== value) {
+      e.target.setCustomValidity("Password and Confirm Password do not match!");
+    } else {
+      e.target.setCustomValidity("");
     }
-    const loginLink = () => {
-        setAction('');
+  };
+
+  //handle login
+  const navigatie = useNavigate();
+
+  const handleLogin = async (event) => {
+    event.preventDefault(); // Ngăn chặn hành vi mặc định của form
+    const formData = new FormData(event.target); // Lấy dữ liệu form
+    const userName = formData.get("username"); // Lấy giá trị của trường Username
+    const password = formData.get("password"); // Lấy giá trị của trường Password
+    console.log("Username:", userName);
+    console.log("Password:", password);
+
+    try {
+      const response = await api.post("login", {
+        username: userName,
+        password: password,
+      });
+      const { token } = response.data;
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(response.data));
+      navigatie("/");
+    } catch (err) {
+      console.log(err);
+      alert("Username or Password is invalid!");
     }
+  };
 
-    // for seeing Password
-    const [see, setSee] = useState(true);
-    const togglePassword = (event) => {
-        setSee(prevState => !prevState);
-    }
-    useEffect(() => {
-        setSee()
-    }
-        , [setSee])
-    // for Confirm Password
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  //handle registration
+  const handleRegitration = (event) => {
+    event.preventDefault(); // Ngăn chặn hành vi mặc định của form
+    const formData = new FormData(event.target); // Lấy dữ liệu form
+    const userName = formData.get("username"); // Lấy giá trị của trường Username
+    const firstName = formData.get("firstname"); // Lấy giá trị của trường Password
+    const lastName = formData.get("lastname"); // Lấy giá trị của trường Password
+    const email = formData.get("email"); // Lấy giá trị của trường Password
+    const password = formData.get("password"); // Lấy giá trị của trường Password
+    console.log("Username:", userName);
+    console.log("Firstname:", firstName);
+    console.log("Lastname:", lastName);
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
 
-    const handleConfirmPasswordChange = (e) => {
-        const value = e.target.value;
-        setConfirmPassword(value);
-
-        if (password !== value) {
-            e.target.setCustomValidity('Password and Confirm Password do not match!');
-        } else {
-            e.target.setCustomValidity('');
-        }
-    };
-
-    //handle login
-    const navigatie = useNavigate()
-
-    const handleLogin = async (event) => {
-        event.preventDefault(); // Ngăn chặn hành vi mặc định của form
-        const formData = new FormData(event.target); // Lấy dữ liệu form
-        const userName = formData.get('username'); // Lấy giá trị của trường Username
-        const password = formData.get('password'); // Lấy giá trị của trường Password
-        console.log("Username:", userName);
-        console.log("Password:", password);
-
-        try{
-            const response = await api.post("login",userName,password);
-            const {token} = response.data;
-            localStorage.setItem("token", token);
-            localStorage.setItem("user",JSON.stringify(response.data));
-            navigatie("/");
-        } catch (err){
-            console.log(err);
-            alert("Username or Password is invalid!");
-        }
-        
-    };
-
-    //handle registration
-    const handleRegitration = (event) => {
-        event.preventDefault(); // Ngăn chặn hành vi mặc định của form
-        const formData = new FormData(event.target); // Lấy dữ liệu form
-        const userName = formData.get('username'); // Lấy giá trị của trường Username
-        const firstName = formData.get('firstname'); // Lấy giá trị của trường Password
-        const lastName = formData.get('lastname'); // Lấy giá trị của trường Password
-        const email = formData.get('email'); // Lấy giá trị của trường Password
-        const password = formData.get('password'); // Lấy giá trị của trường Password
-        console.log("Username:", userName);
-        console.log("Firstname:", firstName);
-        console.log("Lastname:", lastName);
-        console.log("Email:", email);
-        console.log("Password:", password);
-    };
-
-    return (
-        <div className="login-body">
-            <div className={`wrapper ${action}`}>
-                <div className="form-box login">
-                    <form action="" onSubmit={handleLogin}>
-                        <h1>Login</h1>
-                        <div className="input-box">
-                            <input type="text" name="username" placeholder="Username" required></input>
-                            <FaUser className="icon"></FaUser>
-                        </div>
-                        <div className="input-box">
-                            <input type={see ? "text" : "password"} name="password" placeholder="Password" required></input>
-                            {see ?
-                                <FaEyeSlash className="icon" onClick={() => togglePassword()} />
-                                : <FaEye className="icon" onClick={() => togglePassword()} />
-                            }
-                        </div>
-                        <div className="remember-forgot">
-                            <label><input type="checkbox"></input>Remember me</label>
-                            <a href="#">Forgot password?</a>
-                        </div>
-                        <button type="submit">Login</button>
-
-                        <div className="register-link">
-                            <p>Don't have an account? <a href="#" onClick={registerLink}>Register</a></p>
-                        </div>
-                    </form>
-
-                </div>
-
-                <div className="form-box register">
-                    <form action="" onSubmit={handleRegitration}>
-                        <h1>Registraion</h1>
-
-                        <div className="input-box">
-                            <input type="text" placeholder="Username" name="username" required></input>
-                            <FaUser className="icon" />
-                        </div>
-                        <div className="input-box">
-                            <input type="text" placeholder="First name" name="firstname" required></input>
-                            <FaPen className="icon" />
-                        </div>
-                        <div className="input-box">
-                            <input type="text" placeholder="Last name" name="lastname" required></input>
-                            <FaPen className="icon" />
-                        </div>
-                        <div className="input-box">
-                            <input type="email" placeholder="Email" name="email" required></input>
-                            <FaEnvelope className="icon" />
-                        </div>
-                        <div className="input-box">
-                            <input
-                                type={see ? "text" : "password"}
-                                id="password"
-                                value={password}
-                                placeholder="Password"
-                                name="password"
-                                onChange={(e) => setPassword(e.target.value)}
-                                required
-                            />
-                            {see ?
-                                <FaEyeSlash className="icon" onClick={() => togglePassword()} />
-                                : <FaEye className="icon" onClick={() => togglePassword()} />
-                            }
-                        </div>
-                        <div className="input-box">
-                            <input
-                                type={see ? "text" : "password"}
-                                id="confirmPassword"
-                                value={confirmPassword}
-                                placeholder="Confirm password"
-                                onChange={handleConfirmPasswordChange}
-                                required
-                            />
-                            {see ?
-                                <FaEyeSlash className="icon" onClick={() => togglePassword()} />
-                                : <FaEye className="icon" onClick={() => togglePassword()} />
-                            }
-                        </div>
-                        <div className="remember-forgot">
-                            <label><input type="checkbox" required></input>
-                                I agree to the terms & conditions
-                            </label>
-                        </div>
-
-                        <button type="submit">Register</button>
-                        <div className="register-link">
-                            <p>Already have an account? <a href="#" onClick={loginLink}>Login</a></p>
-                        </div>
-                    </form>
-                </div>
+  return (
+    <div className="login-body">
+      <div className={`wrapper ${action}`}>
+        <div className="form-box login">
+          <form action="" onSubmit={handleLogin}>
+            <h1>Login</h1>
+            <div className="input-box">
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                required
+              ></input>
+              <FaUser className="icon"></FaUser>
             </div>
+            <div className="input-box">
+              <input
+                type={see ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                required
+              ></input>
+              {see ? (
+                <FaEyeSlash className="icon" onClick={() => togglePassword()} />
+              ) : (
+                <FaEye className="icon" onClick={() => togglePassword()} />
+              )}
+            </div>
+            <div className="remember-forgot">
+              <label>
+                <input type="checkbox"></input>Remember me
+              </label>
+              <a href="#">Forgot password?</a>
+            </div>
+            <button type="submit">Login</button>
+
+            <div className="register-link">
+              <p>
+                Don't have an account?{" "}
+                <a href="#" onClick={registerLink}>
+                  Register
+                </a>
+              </p>
+            </div>
+          </form>
         </div>
-    );
-}
 
-export default LoginForm
+        <div className="form-box register">
+          <form action="" onSubmit={handleRegitration}>
+            <h1>Registraion</h1>
 
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                required
+              ></input>
+              <FaUser className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="First name"
+                name="firstname"
+                required
+              ></input>
+              <FaPen className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Last name"
+                name="lastname"
+                required
+              ></input>
+              <FaPen className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                required
+              ></input>
+              <FaEnvelope className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type={see ? "text" : "password"}
+                id="password"
+                value={password}
+                placeholder="Password"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              {see ? (
+                <FaEyeSlash className="icon" onClick={() => togglePassword()} />
+              ) : (
+                <FaEye className="icon" onClick={() => togglePassword()} />
+              )}
+            </div>
+            <div className="input-box">
+              <input
+                type={see ? "text" : "password"}
+                id="confirmPassword"
+                value={confirmPassword}
+                placeholder="Confirm password"
+                onChange={handleConfirmPasswordChange}
+                required
+              />
+              {see ? (
+                <FaEyeSlash className="icon" onClick={() => togglePassword()} />
+              ) : (
+                <FaEye className="icon" onClick={() => togglePassword()} />
+              )}
+            </div>
+            <div className="remember-forgot">
+              <label>
+                <input type="checkbox" required></input>I agree to the terms &
+                conditions
+              </label>
+            </div>
+
+            <button type="submit">Register</button>
+            <div className="register-link">
+              <p>
+                Already have an account?{" "}
+                <a href="#" onClick={loginLink}>
+                  Login
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
-const baseURL = ""; /* Your Base URL  */
+const baseURL = "http://localhost:8080/api/"; /* Your Base URL  */
 
 const config = {
-    baseURL: baseURL,
+  baseURL: baseURL,
 };
 
 const api = axios.create(config);
@@ -10,17 +10,16 @@ const api = axios.create(config);
 api.defaults.baseURL = baseURL;
 
 const handleBefore = (config) => {
-
-    const token = localStorage.getItem("token")?.replaceAll('"',"");
-    config.headers["Authorization"] = `Bearer ${token}`;
-    // loại token được sử dụng để xác thực. 
-    // Người gửi yêu cầu (client) "mang" (bear) token này để chứng minh rằng họ đã đăng nhập hoặc có quyền truy cập.
-    return config;
+  const token = localStorage.getItem("token")?.replaceAll('"', "");
+  config.headers["Authorization"] = `Bearer ${token}`;
+  // loại token được sử dụng để xác thực.
+  // Người gửi yêu cầu (client) "mang" (bear) token này để chứng minh rằng họ đã đăng nhập hoặc có quyền truy cập.
+  return config;
 };
 
-api.interceptors.request.use(handleBefore,null);
+api.interceptors.request.use(handleBefore, null);
 // một interceptor để mỗi khi gửi request,
 // hàm handleBefore sẽ được chạy trước,
 // thêm token vào tất cả các request.
 
-export default api
+export default api;
