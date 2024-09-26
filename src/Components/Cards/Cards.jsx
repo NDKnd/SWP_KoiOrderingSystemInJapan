d5ecb149d8f153c0cd80d14c2fc2d1ab7e48ac85
HNThang18 <thanghnse180128@fpt.edu.vn>
2024-09-26 21:35:57 +0700

    Update Cards.jsx

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,62 +1,46 @@
-import { Card, List, Row, Col, Popover } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { Card, Row, Col, Button, Typography } from 'antd';
 import './Cards.css';
+
+const { Title, Paragraph } = Typography;
 
 function Cards(props) {
 
-    const navigate = useNavigate();
+  const dataListCards = props.dataListCards || [];
 
-    const dataListCards = props.dataListCards || {};
-
-    return (
-        <List
-            grid={{
-                gutter: 16,
-                column: 3,
-            }}
-            dataSource={dataListCards}
-            renderItem={(item) => (
-                <List.Item>
-                    <Card
-                        className='cust-card'
-                        style={{
-                            width: '25rem',
-                            height: '6rem',
-                            margin: '0 auto',
-                        }}
-                        hoverable
-                    >
-                        <Row>
-                            <Col span={9}>
-                                <img src={item.img} 
-                                style={{objectFit: 'cover', width: '100%', height: '100%', }} 
-                                />
-                            </Col>
-                            <Col span={15}>
-                                <div style={{ padding: '1em' }}>
-                                    <h3>{item.titleCard}</h3>
-                                    <p>{item.description}</p>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Card>
-                </List.Item>
-            )}
-        />
-    );
+  return (
+    <Row gutter={[16, 16]}>
+      {dataListCards.map((item, index) => (
+        <Col span={8} key={index}>
+          <Card
+          className='cust-card'
+            hoverable
+            cover={
+              <img 
+                src={item.img} 
+                alt={item.titleCard} 
+                style={{ height: '200px', objectFit: 'cover' }}
+              />
+            }
+          >
+            <Title level={4}>{item.titleCard}</Title>
+            <Paragraph>{item.description}</Paragraph>
+            <Button type="primary">Read More</Button>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
 
-
 Cards.defaultProps = {
-    title: 'Default',
-    dataListCards: [
-        {
-            titleCard: 'Card title hehe',
-            description: 'This is the description.',
-            img: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-        },
-    ],
+  title: 'Default',
+  dataListCards: [
+    {
+      titleCard: 'Card title hehe',
+      description: 'This is the description.',
+      img: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
+    },
+  ],
 };
 
 export default Cards;
-
