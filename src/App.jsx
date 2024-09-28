54fc30592f569e7c1c01772f9c3f1d537560a9fc
abc@gmail.com <khoind2004@gmail.com>
2024-09-28 11:22:06 +0700

    App update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Error from "./Pages/Error/Error.jsx";
 import LoginForm from "./Pages/Login/LoginForm.jsx";
 import Home from "./Pages/Home/Home.jsx";
 import Manager from "./Pages/Manager/Manager.jsx";
 import PrivateRoute from "./Components/private-rout/PrivateRoute.jsx";
+import Account from "./Pages/Account/Account.jsx";
+import KoiPageFind from "./Pages/Kois/KoiPageFind.jsx";
 function App() {
-  
   const router = createBrowserRouter([
     {
       path: "/",
@@ -19,22 +19,39 @@ function App() {
       errorElement: <Error />,
     },
     {
+      path: "KoiPageFind",
+      element: <KoiPageFind />,
+      errorElement: <Error />,
+    },
+    {
       path: "admin",
       element: <PrivateRoute />,
-      children: [{
+      children: [
+        {
           path: "",
-          element : <Manager/>,
-        }
+          element: <Manager />,
+        },
+      ],
+      errorElement: <Error />,
+    },
+    {
+      path: "profile",
+      element: <PrivateRoute />,
+      children: [
+        {
+          path: "",
+          element: <Account />,
+        },
       ],
       errorElement: <Error />,
     },
     {
       path: "*",
       element: <Error />,
-    }
-  ])
+    },
+  ]);
 
   return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
