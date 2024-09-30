d99dd2d87c2c29ec4ba954557cd26c2afbc3c1f7
abc@gmail.com <khoind2004@gmail.com>
2024-09-30 10:56:06 +0700

    Change layout of contentcard

--- src/Components/Content/ContentCard.jsx
+++ src/Components/Content/ContentCard.jsx
@@ -1,11 +1,21 @@
-import { Divider } from "antd";
-import Cards from "../Cards/Cards";
+import { Divider, Row, Col, Card, Typography, Button, Pagination } from "antd";
+import PropTypes from "prop-types";
+// import { useState } from "react";
 
-function ContentCard(props) {
-  const title = props.title;
+// Import các thành phần cần thiết từ antd
+const { Title, Paragraph } = Typography;
 
-  const dataList = props.dataList || {};
-
+function ContentCard({
+  title = "Default",
+  dataList = [
+    {
+      titleCard: "Default Card",
+      description: "This is the default description for the card.",
+      img: "https://source.unsplash.com/random/200x300",
+    },
+  ],
+}) {
+  const currentData = dataList.slice(0, 6);
   return (
     <>
       <Divider
@@ -14,19 +24,58 @@ function ContentCard(props) {
       >
         {title}
       </Divider>
-      <Cards dataListCards={dataList} />
+      <Row gutter={[16, 16]}>
+        {currentData.map((item, index) => (
+          <Col span={8} key={index}>
+            <Card
+              className="cust-card"
+              hoverable
+              cover={
+                <div
+                  style={{
+                    height: "200px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <img
+                    src={item.img}
+                    alt={item.titleCard}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                  />
+                </div>
+              }
+            >
+              <Title level={4}>{item.titleCard}</Title>
+              <Paragraph>{item.description}</Paragraph>
+              <Button
+                type="primary"
+                style={{ margin: "0 auto", display: "block" }}
+              >
+                Order
+              </Button>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </>
   );
 }
 
-ContentCard.defaultProps = {
-  title: "Default",
-  dataList: [],
-};
-
 ContentCard.propTypes = {
-  title: "string",
-  dataList: "array",
+  title: PropTypes.string,
+  dataList: PropTypes.arrayOf(
+    PropTypes.shape({
+      titleCard: PropTypes.string,
+      description: PropTypes.string,
+      img: PropTypes.string,
+    }).isRequired
+  ),
 };
 
 export default ContentCard;
--- src/Components/Content/ContentTable.jsx
+++ src/Components/Content/ContentTable.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useState } from "react";
+// import React from "react";
+// import { useState } from "react";
 import { Divider } from "antd";
 import MyTables from "../Table/Tables";
 
