6d42e2af4d2098cb9527e850a2cfaf2cb691abac
HNThang18 <thanghnse180128@fpt.edu.vn>
2024-10-30 23:05:37 +0700

    update carousel and home page
    need to do more

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,51 +1,60 @@
 import { Carousel } from "antd";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Carousels() {
+  const navigate = useNavigate();
+
   const contentList = [
     {
-      title: "Japna Buy Trip",
-      indx: 0,
+      title: "Koi",
+      image: "https://firebasestorage.googleapis.com/v0/b/koiorderingjapan.appspot.com/o/Carousel%2FCnP_30102024_214858.png?alt=media&token=18dbe326-b903-4ae8-898f-5c90d3bebeaf",
+      index: 0,
+      path: "KoiPageFind",
     },
     {
-      title: "Hello",
-      indx: 1,
+      title: "Farm",
+      image: "https://firebasestorage.googleapis.com/v0/b/koiorderingjapan.appspot.com/o/Carousel%2FCnP_30102024_215153.png?alt=media&token=3d94e1b9-bbb5-4bc1-ba2f-6e8835003417",
+      index: 1,
+      path: "FarmFindPage",
     },
     {
-      title: "hello3",
-      indx: 2,
+      title: "Trip",
+      image: "https://firebasestorage.googleapis.com/v0/b/koiorderingjapan.appspot.com/o/Carousel%2FCnP_30102024_215238.png?alt=media&token=e1529215-d8fe-4647-877a-ac6c32dffe29",
+      index: 2,
+      path: "TripPage",
     },
   ];
 
-  const heightSlide = "60vh";
-  const urlPicBackGround =
-    "https://i.pinimg.com/564x/25/6f/64/256f64b9362c3f834e59f3bc27683c74.jpg";
-
-  const contentStyle = {
-    height: heightSlide,
-    lineHeight: heightSlide,
-
-    color: "var(--purple5)",
-    fontSize: "3em",
-    textShadow: "0 0 5px rgba(1, 0, 0, 1)",
-
-    textAlign: "center",
-    backgroundImage: `url(${urlPicBackGround})`,
-
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backdropFilter: "blur(10px) saturate(200%)",
+  const handleNavigation = (path) => {
+    navigate(path);
   };
+
+  const heightSlide = "60vh";
 
   return (
     <div style={{ height: heightSlide }}>
-      <Carousel startIndex={0} autoplaySpeed={4000} autoplay arrows>
-        {" "}
-        {contentList.map((content, indx) => (
-          <div key={indx}>
-            <h3 style={contentStyle}>
+      <Carousel autoplay fade autoplaySpeed={4000}>
+        {contentList.map((content, index) => (
+          <div key={index} style={{ position: "relative", height: heightSlide, cursor: "pointer" }} onClick={() => handleNavigation(content.path)}>
+            <div
+              style={{
+                height: heightSlide,
+                lineHeight: heightSlide,
+                color: "white",
+                fontSize: "3em",
+                textShadow: "0 0 10px rgba(0, 0, 0, 0.8)",
+                textAlign: "center",
+                backgroundImage: `url(${content.image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                filter: "brightness(0.7)",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <span>{content.title}</span>
-            </h3>
+            </div>
           </div>
         ))}
       </Carousel>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,8 @@
 import "./Home.css";
 import Header from "../../Components/Header/Header";
-import MyTables from "../../Components/Table/Tables";
 import Carousels from "../../Components/Carousel/Carousel";
 import Footers from "../../Components/Footer/Footers";
-import { Divider, Layout } from "antd";
+import { Card, Col, Layout, Row } from "antd";
 import ContentCard from "../../Components/Content/ContentCard";
 import api from "./../../services/axios";
 import { useEffect, useState } from "react";
@@ -43,20 +42,56 @@ function Home() {
   return (
     <Layout>
       <Header />
-      <Layout style={{ padding: "5px 24px 24px" }}>
-        <Carousels />
+      <Carousels />
+      <Layout style={{ padding: "24px" }}>
+        <Row gutter={[16, 16]} style={{ marginBottom: "24px" }}>
+          <Col xs={24} md={12}>
+            <Card className="info-card">
+              <p>
+                Welcome to the Koi Ordering System (KOS)! Explore and book trips
+                to beautiful Koi farms in Japan, discover various Koi breeds, and
+                experience the fascinating world of Koi fish culture.
+              </p>
+            </Card>
+          </Col>
+          <Col xs={24} md={12}>
+            <Card cover={<img alt="Introduction" className="fixed-height-image" src={"https://firebasestorage.googleapis.com/v0/b/koiorderingjapan.appspot.com/o/HomePage%2FCnP_30102024_225431.png?alt=media&token=6f42ba5a-8d7f-464c-98c1-77bacd9088cd"} />}></Card>
+          </Col>
+        </Row>
 
-        {/* <ContentCard title="List Farm" dataList={dataCard} /> */}
-        {/* for 2 type of list : "koi" and "farm" */}
-        {/* <ContentCard title="List Farms" dataList={FarmList} typeList="koi" /> */}
+        <Row gutter={[16, 16]} style={{ marginBottom: "24px" }}>
+          <Col xs={24} md={12}>
+            <Card cover={<img alt="Purpose" className="fixed-height-image" src={"https://firebasestorage.googleapis.com/v0/b/koiorderingjapan.appspot.com/o/HomePage%2FCnP_30102024_225516.png?alt=media&token=a905dfe1-f3d1-408e-b99f-206231ec28d7"} />}></Card>
+          </Col>
+          <Col xs={24} md={12}>
+            <Card className="info-card">
+              <p>
+                The purpose of the Koi Ordering System is to provide a seamless
+                platform for users to browse, search, and book trips to Koi farms
+                in Japan. Users can create custom itineraries, manage bookings,
+                and even purchase Koi fish to be shipped home as part of the
+                experience.
+              </p>
+            </Card>
+          </Col>
+        </Row>
 
-        <Divider
-          orientation="left"
-          style={{ margin: "20px 0", fontSize: "25px" }}
-        >
-          Famous Table
-        </Divider>
-        <MyTables />
+        <Row gutter={[16, 16]}>
+          <Col xs={24} md={12}>
+            <Card className="info-card">
+              <p>
+                Our services include booking trips to renowned Koi farms, creating
+                custom trip itineraries, managing booking processes, offering
+                optional Koi fish purchasing services, and processing payments. We
+                also provide a feedback and rating system to enhance customer
+                experience.
+              </p>
+            </Card>
+          </Col>
+          <Col xs={24} md={12}>
+            <Card cover={<img alt="Service"  className="fixed-height-image" src={"https://firebasestorage.googleapis.com/v0/b/koiorderingjapan.appspot.com/o/HomePage%2FCnP_30102024_225634.png?alt=media&token=5630ac78-a5d3-490f-a233-fda3ab55933f"} />}></Card>
+          </Col>
+        </Row>
       </Layout>
       <Footers style={{ marginTop: "20px" }} />
     </Layout>
